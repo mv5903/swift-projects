898eedbec0bf6f8850534ad1c18baafaaafe42a4
Matthew Vandenberg <mv5903@gmail.com>
2021-10-28 17:37:29 -0700

    Initial Commit

---
+++ Dummy Scoreboard/Dummy_ScoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dummy_ScoreboardApp.swift
+//  Dummy Scoreboard
+//
+//  Created by Matthew Vandenberg on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Dummy_ScoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
